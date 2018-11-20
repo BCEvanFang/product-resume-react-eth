316f8f0e826407df09a650cb203db8aec40cba57
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-11-20 16:20:50 +0800

    Add save/get function

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+// import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import ProductResumeContract from "./contracts/ProductResume.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
 
 import "./App.css";
 
 class App extends Component {
-  state = { 
-    storageValue: 0, 
-    web3: null, 
-    accounts: null, 
-    contract: null ,
-    contractVersion: ""
+  state = {
+    storageValue: 0,
+    web3: null,
+    accounts: null,
+    contract: null,
+    //
+    productId: "",
+    productName: "",
+    productInfo: "",
+    // 從鏈上取得的產品資訊
+    pruductFromChain: {
+      id: "",
+      name: "",
+      info: ""
+    }
   };
 
   componentDidMount = async () => {
@@ -23,13 +33,15 @@ class App extends Component {
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
+      const Contract = truffleContract(ProductResumeContract);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      // this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }, this.getProductFromChain);
+
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -39,47 +51,95 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  // 儲存產品資訊
+  handleSave = async () => {
 
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
+    const { accounts, contract } =  this.state;
+    const { productId, productName, productInfo } =  this.state;
+
+    // Get the contract version
+    try {
+      let result = await contract.save(productId, productName, productInfo, { from: accounts[0] });
+      console.log(result)
+
+      // 更新資料
+      this.getProductFromChain();
+
+    } catch(e) {
+      console.error(e);
+      alert("save failed");
+    }
+  }
+
+  handleProductIdChange = (event) => {
+    this.setState({productId: event.target.value});
+  }
+
+  handleProductNameChange = (event) => {
+    this.setState({productName: event.target.value});
+  }
+
+  handleProductInfoChange = (event) => {
+    this.setState({productInfo: event.target.value});
+  }
+
+  // 從區塊鏈上取得產品資訊
+  getProductFromChain = async () => {
+    const { contract } = this.state;
 
     // Get the value from the contract to prove it worked.
     const response = await contract.get();
 
-    // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
-
-    // Get the contract version
-    const version = await contract.getVersion();
-
-    this.setState({ contractVersion: version });
-  };
+    this.setState({pruductFromChain: {
+      id: response[0],
+      name: response[1],
+      info: response[2]
+    }});
+  }
 
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 37</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className="container">
         
-        <hr />
+        <h1>
+          Product Resume
+        </h1>
+
+        <form className="form">
+          <div className="form-group">
+            <label>Product Id</label>
+            <input type="text" className="form-control mx-sm-3" value={this.state.productId} onChange={this.handleProductIdChange}/>
+          </div>
+          <div className="form-group">
+            <label>Product Name</label>
+            <input type="text" className="form-control mx-sm-3" value={this.state.productName} onChange={this.handleProductNameChange}/>
+          </div>
+          <div className="form-group">
+            <label>Product Description</label>
+            <input type="text" className="form-control mx-sm-3" value={this.state.productInfo} onChange={this.handleProductInfoChange}/>
+          </div>
+          
+          <button type="button" className="btn btn-primary" onClick={this.handleSave}>Save Product</button>
+        </form>
+        
+        <hr/>
+
+        <h1>
+          Product Information
+        </h1>
+
         <div>
-          Version: {this.state.contractVersion}
+          Product Id: <span>{this.state.pruductFromChain.id}</span>
         </div>
-        
+        <div>
+          Product Name: <span>{this.state.pruductFromChain.name}</span>
+        </div>
+        <div>
+          Product Information: <span>{this.state.pruductFromChain.info}</span>
+        </div>
       </div>
     );
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+
+// module.exports = function(deployer) {
+//   deployer.deploy(SimpleStorage);
+// };
+
+
+var ProductResume = artifacts.require("./ProductResume.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(ProductResume);
 };
