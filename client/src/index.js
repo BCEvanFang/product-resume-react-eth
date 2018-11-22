216cb7b0bb699b239a41e5aa4789b2682aa32411
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-11-22 14:00:01 +0800

    Add header and footer

--- client/src/index.js
+++ client/src/index.js
@@ -14,11 +14,24 @@ import { BrowserRouter, Route } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter>
     <div>
+      
+      {/* Header */}
+      <div style={{height:"40px"}}></div>
+
       <Route exact path="/" component={ProductList} />
       <Route path="/query" component={Query} />
       <Route path="/add" component={AddProduct} />
       <Route path="/list" component={ProductList} />
       <Route path="/edit/:seq" component={EditProduct} />
+
+      {/* Footer */}
+      <div>
+        <hr style={{width:"40px"}} />
+        <div style={{textAlign:"center"}}>
+          <span className="text-muted">Product Resume Blockchain System</span>
+        </div>
+      </div>
+
     </div>
   </BrowserRouter>,
   document.getElementById("root")
